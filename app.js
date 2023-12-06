1483f805e71ec21bf80a521732b6124073c36333
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-06 13:41:38 +0400

    Left:only:underlines:and:selectors

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const inputBox = document.getElementById('inputBox')
 const list = document.getElementById('list')
 const underLine = document.querySelector('.underline')
 const sunIcon = document.querySelector('.Sun') 
+const Itemsleft = document.getElementById('Itemsleft')
+const ClearComplited = document.getElementById('ClearComplited')
+const ShowAll = document.getElementById('ShowAll')
+const onlyActive = document.getElementById('onlyActive')
+const onlyCompleted = document.getElementById('onlyCompleted')
 
 sunIcon.onclick = function(){
     document.body.classList.toggle('light-mode')
@@ -11,18 +16,26 @@ sunIcon.onclick = function(){
         sunIcon.src = '/assets/Sun.svg'
     }
 }
-
 function gela(){
     if(inputBox.value === ''){
-        alert('Pls write something bzshy !')
+        console.log('Entered')
     }else {
         let li = document.createElement('li')
         li.innerHTML = inputBox.value
         list.appendChild(li)
+        let span = document.createElement('span')
+        span.innerHTML = 'x'
+        li.appendChild(span)
     }
     inputBox.value = ''
 }
-
+list.addEventListener('click', function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked')
+    }else if (e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove()
+    }
+})
 document.querySelector('.inputText').addEventListener('keydown', function(e) {
     if (e.key === 'Enter') {
         gela()
